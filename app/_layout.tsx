380cbadbc819865c9e2af09fdd90fb08b0e0f392
Maddisen Mohnsen <memohnsen@gmail.com>
2026-03-08 21:50:24 -0400

    hero ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from 'expo-router'
 import '../global.css'
+import { HeroUINativeProvider } from 'heroui-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function RootLayout() {
-  return <Stack />
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <Stack />
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
+  )
 }
