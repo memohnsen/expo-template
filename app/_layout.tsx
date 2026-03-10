8ea52d8aaa832ff52d32fe1e088cf0a4af852ab6
Maddisen Mohnsen <memohnsen@gmail.com>
2026-03-10 16:23:20 -0400

    hide hero ui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,14 @@
 import { Stack } from 'expo-router'
 import '../global.css'
-import { HeroUINativeProvider } from 'heroui-native'
+// import { HeroUINativeProvider } from 'heroui-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <HeroUINativeProvider>
-        <Stack />
-      </HeroUINativeProvider>
+      {/* <HeroUINativeProvider> */}
+      <Stack />
+      {/* </HeroUINativeProvider> */}
     </GestureHandlerRootView>
   )
 }
