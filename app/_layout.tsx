cf21de9df00d4f81f6d1cb7afbdc9b1518a40eb8
Maddisen Mohnsen <memohnsen@gmail.com>
2026-03-12 17:41:05 -0400

    colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       {/* <HeroUINativeProvider> */}
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }} />
       {/* </HeroUINativeProvider> */}
     </GestureHandlerRootView>
   )
