8a9fd5b35def18af7fe771cc4d87b4d5b2dd7146
Maddisen Mohnsen <memohnsen@gmail.com>
2026-04-06 15:49:34 -0400

    fix

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,10 @@
+const path = require('path')
 const { getDefaultConfig } = require('expo/metro-config')
 const { withUniwindConfig } = require('uniwind/metro')
 
 const config = getDefaultConfig(__dirname)
 
 module.exports = withUniwindConfig(config, {
-  cssEntryFile: '@/global.css',
-  dtsFile: '@/src/app/uniwind-types.d.ts',
+  cssEntryFile: './global.css',
+  dtsFile: path.resolve(__dirname, 'src/app/uniwind-types.d.ts'),
 })
