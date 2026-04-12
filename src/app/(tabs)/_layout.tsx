17bf80e0b9d4610117d2e3d65aed5ff74e60dc95
Maddisen Mohnsen <memohnsen@gmail.com>
2026-04-12 13:52:47 -0400

    folders

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -13,15 +13,8 @@ export default function TabsLayout() {
       <NativeTabs.Trigger name="tabs2">
         <NativeTabs.Trigger.Label hidden />
         <NativeTabs.Trigger.Icon
-          sf="list.bullet"
-          md="list"
-        />
-      </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="settings">
-        <NativeTabs.Trigger.Label hidden />
-        <NativeTabs.Trigger.Icon
-          sf="gearshape"
-          md="settings"
+          sf={{ default: 'camera', selected: 'camera' }}
+          md="photo_camera"
         />
       </NativeTabs.Trigger>
     </NativeTabs>
